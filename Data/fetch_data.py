1dd207f11d8e69fb9a815e8c566e21b9e775e503
aceros123 <57160341+aceros123@users.noreply.github.com>
2025-01-02 23:06:31 -0500

    Consumo de APi datos abiertos
    https://www.datos.gov.co/Salud-y-Protecci-n-Social/Calidad-del-Agua-para-Consumo-Humano-en-Colombia/nxt2-39c3/data_preview

---
+++ Data/fetch_data.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def fetch_data():
+    API_URL = "https://www.datos.gov.co/resource/nxt2-39c3.json?$limit=1000000"
+    print(API_URL)
+    try:
+        response = requests.get(API_URL, timeout=60)
+        if response.status_code == 200:
+            print("Datos recibidos correctamente.")
+            data = response.json()
+            print(data)
+        else:
+            print(
+                f"Error al obtener los datos: {response.status_code} - {response.reason}")
+    except Exception as e:
+        print(f"Error al obtener los datos: {e}")
+
+    return data
+
+
+pass
