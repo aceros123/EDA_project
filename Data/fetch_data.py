250a43a1830c48eab4c010863fcda0eb1661859f
aceros123 <danielacero@unisangil.edu.co>
2025-01-05 18:28:42 -0500

    Analisis de IRCA en colombia

--- Data/fetch_data.py
+++ Data/fetch_data.py
@@ -3,13 +3,11 @@ import requests
 
 def fetch_data():
     API_URL = "https://www.datos.gov.co/resource/nxt2-39c3.json?$limit=1000000"
-    print(API_URL)
     try:
         response = requests.get(API_URL, timeout=60)
         if response.status_code == 200:
             print("Datos recibidos correctamente.")
             data = response.json()
-            print(data)
         else:
             print(
                 f"Error al obtener los datos: {response.status_code} - {response.reason}")
